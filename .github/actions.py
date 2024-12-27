6a7ecb981e88475741327cbd1ebd2b6a6d6036a9
Haim Shulner <haim.shulner@gmail.com>
2024-12-27 11:25:33 +0200

    clear names

--- .github/actions.py
+++ .github/actions.py
@@ -1,15 +1,9 @@
-import json
 import os
 
-def metadata():
-     pass
 
 def main():
     action = os.environ['PKG_ACTION']
-
-    if action == 'METADATA':
-        metadata()
-    
+    pass
    
 if __name__ == '__main__':
     main()
